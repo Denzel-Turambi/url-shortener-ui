8fb359f6a04ce2dcffa72809bf4740b1009538d0
Denzel Turambi <dturambi26@gmail.com>
2023-09-25 09:35:35 -0600

    feat: Create UrlCard component

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './UrlContainer.css';
+import UrlCard from '../UrlCard/UrlCard';
 
 const UrlContainer = props => {
   console.log(props.urls)
   const urlEls = props.urls.map(url => {
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
+      // <div className="url">
+      //   <h3>{url.title}</h3>
+      //   <a href={url.short_url} target="blank">{url.short_url}</a>
+      //   <p>{url.long_url}</p>
+      // </div>
+      <UrlCard 
+        title={url.title}
+        short_url={url.short_url}
+        long_url={url.long_url}
+        id={url.id}
+        key={url.id}
+      />
     )
   });
 
